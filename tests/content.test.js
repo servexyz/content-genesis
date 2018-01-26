055289f8226652680503eb06997e184e80d5ca21
alechp <me@alechp.com>
2018-01-26 11:06:51 -0800

    Finished switching to fp paradigm for string interpolation. The code is so much purrrtier

--- tests/content.test.js
+++ tests/content.test.js
@@ -3,14 +3,21 @@
  * @Date:   2018-01-24T16:36:38-08:00
  * @Email:  alec@bubblegum.academy
  * @Last modified by:   alechp
- * @Last modified time: 2018-01-25T13:06:58-08:00
+ * @Last modified time: 2018-01-26T11:06:17-08:00
  */
 
 const log = console.log;
-test("interpolate() works", () => {
-  let { interpolate } = require("../src/content.js");
-  const { TEMPLATE } = require("../templates/sample.template.js");
-  let vars = { component: "FooBar" };
-  log(TEMPLATE);
-  interpolate(TEMPLATE, vars);
+// test("interpolate() works", () => {
+//   let { interpolate } = require("../src/content.js");
+//   const { TEMPLATE } = require("../templates/sample.template.js");
+//   let vars = { component: "FooBar" };
+//   log(TEMPLATE);
+//   interpolate(TEMPLATE, vars);
+// });
+test("abc", () => {
+  const { abc, inted, inted2 } = require("../src/content.js");
+  log(abc);
+  log(`abc: ${abc}`);
+  log(`inted: ${inted}`);
+  log(`inted: ${inted2}`);
 });
